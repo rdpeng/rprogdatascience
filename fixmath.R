b5e8761cd165fb1133dface4fed207e3223b37fb
Roger D. Peng [amelia] <rdpeng@gmail.com>
2016-06-28 08:45:43 -0400

    Read commandArgs

--- fixmath.R
+++ fixmath.R
@@ -1,10 +1,12 @@
+cargs <- commandArgs(TRUE)
+infile <- cargs[1]
 
 fixmath <- function(infile) {
         doc0 <- readLines(infile)
-        
         doc <- sub("^\\\\\\[$", "{\\$\\$}", doc0, perl = TRUE)
         doc <- sub("^\\\\\\]$", "{\\/\\$\\$}", doc, perl = TRUE)
         doc <- gsub("\\$(\\S+)\\$", "{\\$\\$}\\1{\\/\\$\\$}", doc, perl = TRUE)
-        
         writeLines(doc, infile)
 }
+
+fixmath(infile)
