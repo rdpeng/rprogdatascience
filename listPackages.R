11cb03f12123e228acb241a895cda78513e94195
Roger D. Peng [amelia] <rdpeng@gmail.com>
2016-12-13 08:23:24 -0500

    Updated code for listing all packages

--- listPackages.R
+++ listPackages.R
@@ -1,16 +1,25 @@
 library(dplyr, warn.conflicts = FALSE)
-pkgs <- local({
+local({
         files <- dir(pattern = glob2rx("*.Rmd"))
         code <- lapply(files, readLines) %>% unlist
         r <- regexec("^library\\((.*?)\\)", code, perl = TRUE)
         m <- regmatches(code, r)
         u <- sapply(m, length) > 0
-        sapply(m[u], function(x) x[2]) %>% unique
+        pkgs1 <- sapply(m[u], function(x) x[2])
+        r <- regexec("([A-Za-z]+)::.*", code, perl = TRUE)
+        m <- regmatches(code, r)
+        u <- sapply(m, length) > 0
+        pkgs2 <- sapply(m[u], function(x) x[2])
+        pkgs <- unique(c(pkgs1, pkgs2)) %>% sort
+        
+        writeLines(pkgs, "_R_package_list.txt")
+        
+        int <- installed.packages()[, 1]
+        ava <- available.packages() %>% rownames
+        need <- setdiff(pkgs, int)
+        need <- need[need %in% ava]
+        if(length(need) > 0L) {
+                install.packages(need)
+        }
 })
-writeLines(pkgs, "_R_package_list.txt")
 
-int <- installed.packages()[, 1]
-need <- setdiff(pkgs, int)
-if(length(need) > 0L) {
-        install.packages(need)
-}
